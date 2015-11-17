1be2e12597eacfbeea66be1d4aa3c3791841bd20
mackee28 <markus_r@icloud.com>
2015-11-17 20:39:54 +0100

    add transparency to mobile-menu

--- dist/js/hello_world.js
+++ dist/js/hello_world.js
@@ -1,11 +1,13 @@
 
 function tell(){
 //alert('test');
-    if(document.getElementById("preorlogin").innerHTML == "Logga in"){
+    if(document.getElementById("preorlogin").innerHTML == "Logga in" || document.getElementById("preorlogin-mobile").innerHTML == "Logga in"){
         document.getElementById("preorlogin").innerHTML = "Premium";
+        document.getElementById("preorlogin-mobile").innerHTML = "Premium";
         document.getElementById("premium-banner-text").innerHTML = "2 för 1";
     }else{
         document.getElementById("preorlogin").innerHTML = "Logga in";
+        document.getElementById("preorlogin-mobile").innerHTML = "Logga in";
         document.getElementById("premium-banner-text").innerHTML = "Premium";
     }
     
